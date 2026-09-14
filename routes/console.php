6e80767651a94b81e5eb87c3a7bdbee071b53943
Yasser <yassergharieb14@gmail.com>
2026-09-15 01:26:29 +0300

    hot fix of test coverage

--- routes/console.php
+++ routes/console.php
@@ -5,11 +5,6 @@ use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\DB;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
-
-
 
 Schedule::useCache('database');
 
@@ -17,7 +12,6 @@ Schedule::command('idempotency-keys:prune')
     ->hourly()
     ->withoutOverlapping()
     ->onOneServer();
-
 Schedule::command('s3:cleanup-documents')
     ->dailyAt('03:00')
     ->withoutOverlapping();
